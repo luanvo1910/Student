6c818a757ca1dd194ed8ce9d0acf42f291387ea3
luan <oga.tatsumi1230410@gmail.com>
2021-11-30 10:12:27 +0700

    Add project files.

---
+++ Student/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Student
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student student1 = new Student();
+
+            student1.Name = Console.ReadLine();
+            student1.Age = int.Parse(Console.ReadLine());
+            student1.GPA = float.Parse(Console.ReadLine());
+
+            Console.WriteLine($"{student1.Name} {student1.Age} Years old, {student1.GPA} points");
+            Console.ReadLine();
+        }
+    }
+}
